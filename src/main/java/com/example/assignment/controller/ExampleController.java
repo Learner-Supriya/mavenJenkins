82e354e85503a8d0f90be1d57cbc260411fb4e73
unknown <supriyaede16@gmail.com>
2022-07-07 14:02:02 +0530

    hello

---
+++ src/main/java/com/example/assignment/controller/ExampleController.java
@@ -0,0 +1,39 @@
+package com.example.assignment.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.assignment.pojoClass.greatLearning;
+
+	@RestController
+	public class ExampleController {
+		
+		@GetMapping("/info")
+//		@ResponseBody
+		public greatLearning get() {
+			greatLearning gl=new greatLearning();
+			gl.setCourseId("1010");
+			gl.setCourseName("ML");
+			gl.setInstructor("samarth");
+			return gl;
+		}
+		
+		@PostMapping("custominfo")
+		public greatLearning customInfo(String CourseType,String CourseId,String Instructor) {
+			greatLearning gl=new greatLearning();
+			gl.setCourseId(CourseId);
+			gl.setCourseName(CourseType);
+			gl.setInstructor(Instructor);
+			return gl;
+		}
+		
+
+		
+
+	}
+
+
---
+++ src/main/java/com/example/assignment/pojoClass/greatLearning.java
@@ -0,0 +1,12 @@
+package com.example.assignment.pojoClass;
+import lombok.Data;
+
+@Data
+public class greatLearning {
+	
+	
+	
+		private String instructor;
+		private String courseId;
+		private String courseName;
+}
